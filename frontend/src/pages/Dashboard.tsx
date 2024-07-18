118a41212451806d86f5e42954f7c701e55070d9
YajatKaul <68262974+yajatkaul@users.noreply.github.com>
2024-07-18 23:54:49 +0530

    modified:   frontend/src/pages/Dashboard.tsx

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -3,10 +3,13 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import OrderCard from "../components/OrderCard";
 import useGetAllOrders from "../hooks/useGetAllOrders";
+import { FaPrint } from "react-icons/fa";
 
 const Dashboard = () => {
   const { orders } = useGetAllOrders();
-
+  const print = () => {
+    window.print();
+  };
   return (
     <>
       <div className="flex flex-col h-screen bg-cover bg-center bg-[url('/bg.jpg')]">
@@ -25,6 +28,11 @@ const Dashboard = () => {
               />
             );
           })}
+          <div className="fixed bottom-[40px] right-[50px]">
+            <button onClick={print} className="btn">
+              <FaPrint className="w-[40px] h-[40px]" />
+            </button>
+          </div>
         </div>
         <Footer />
       </div>
