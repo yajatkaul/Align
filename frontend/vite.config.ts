6e94adfd619e6a1ddecc46ab09a0e8560d8e8b3e
YajatKaul <68262974+yajatkaul@users.noreply.github.com>
2024-07-22 13:42:15 +0530

    modified:   frontend/.gitignore 	modified:   frontend/package-lock.json     modified:   frontend/package.json 	modified:   frontend/vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,17 @@
+// @ts-nocheck
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+const PORT = process.env.PORT;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: PORT || 1234,
     proxy: {
       "/api": {
         target: "http://localhost:5000",
