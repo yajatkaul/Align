886b0ddc9aa4d93c5a578619b285c6519063fcfd
YajatKaul <68262974+yajatkaul@users.noreply.github.com>
2024-07-22 14:18:25 +0530

    modified:   frontend/vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     host: true,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://align-backend.onrender.com/",
       },
     },
   },
