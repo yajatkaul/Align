09fb44c1348adf613eee0b119d4190b85931b0d7
YajatKaul <68262974+yajatkaul@users.noreply.github.com>
2024-10-04 18:33:02 +0530

    modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.json());
 
